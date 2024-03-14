01c188c7e564c183193ed507ab0f049fea28f370
Akash Kanetkar <akashkanetkar1996@gmail.com>
2024-03-14 13:46:54 +0100

    initial commit1

--- scripts/script.js
+++ scripts/script.js
@@ -9,38 +9,17 @@ if ('serviceWorker' in navigator) {
 }
 
 const video = document.getElementById("video");
-const statusElement = document.getElementById("status");
-const startButton = document.getElementById("startButton");
-const waitingTimeout = 7000; // 7 seconds
-const displayDelay = 6500; // 6.5 seconds
-
-let lastFaceDetectionTime = 0;
-let waitingTimeoutId;
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/facepwa/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/facepwa/models"),
-]).then(() => {
-  // Wait for the start button click to initialize the webcam and face detection
-  startButton.addEventListener("click", startSeatOccupancy);
-});
+]).then(startWebcam);
 
 function startWebcam() {
   navigator.mediaDevices
-    .enumerateDevices()
-    .then((devices) => {
-      const videoDevices = devices.filter((device) => device.kind === "videoinput");
-
-      if (videoDevices.length > 0) {
-        const deviceId = videoDevices[0].deviceId; // Change index if needed
-        return navigator.mediaDevices.getUserMedia({
-          video: { deviceId: { exact: deviceId } },
-          audio: false,
-        });
-      } else {
-        console.error("No video devices found.");
-        throw new Error("No video devices found.");
-      }
+    .getUserMedia({
+      video: true,
+      audio: false,
     })
     .then((stream) => {
       video.srcObject = stream;
@@ -50,53 +29,27 @@ function startWebcam() {
     });
 }
 
-function startSeatOccupancy() {
-  startWebcam();
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  faceapi.matchDimensions(canvas, { height: video.height, width: video.width });
 
-  video.addEventListener("play", () => {
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-    faceapi.matchDimensions(canvas, { height: video.height, width: video.width });
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks();
 
-    setInterval(async () => {
-      const detections = await faceapi
-        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks();
+    const resizedDetections = faceapi.resizeResults(detections, {
+      height: video.height,
+      width: video.width,
+    });
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
 
-      const currentTime = Date.now();
-
-      if (detections.length > 0) {
-        // Face detected
-        lastFaceDetectionTime = currentTime;
-        showOccupiedStatus();
-      } else {
-        // No face detected
-        const elapsedTimeSinceLastDetection = currentTime - lastFaceDetectionTime;
-
-        if (elapsedTimeSinceLastDetection < waitingTimeout) {
-          // Still within the waiting timeout, show "Waiting..."
-          showWaitingStatus();
-        } else {
-          // Waiting timeout exceeded, show "Seat not occupied"
-          showNotOccupiedStatus();
-        }
-      }
-    }, 100); // Set the interval duration to 100 milliseconds
-  });
-}
-
-function showOccupiedStatus() {
-  statusElement.textContent = "Seat occupied";
-}
-
-function showNotOccupiedStatus() {
-  statusElement.textContent = "Seat not occupied";
-}
-
-function showWaitingStatus() {
-  statusElement.style.display = "block"; // Show the status element
-  statusElement.textContent = "Waiting...";
-}
+    console.log(detections);
+  }, 100);
+});
 
 
 
